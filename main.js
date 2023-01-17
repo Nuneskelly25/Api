fe0fa36f9c0cff30c2f87b3243244997d84a5d38
Nuneskelly25 <107558150+Nuneskelly25@users.noreply.github.com>
2023-01-17 20:34:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-posts = document.querySelector('#posts')
+var posts = document.querySelector('#posts')
 
-function loadPosts(){
-    console.log('clicou')
+async function loadPosts(){
+    posts.innerHTML = "Carregando..."
+
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    let json = await req.json()
+    montarBlog(json)
+}
+function montarBlog(lista){
+    let html = ''
+
+    for(let i in lista){
+
+     html += posts.innerHTML = `<div class = 'box'> <h3> ${lista[i].title}</h3> <p> ${lista[i].body}</p> </div>`    
+  
+    }
+
+    posts.innerHTML= html
+
+
 }
